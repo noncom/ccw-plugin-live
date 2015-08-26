95aebe01ac0999b2b8d1ca87e8d4647febd859f7
noncom <alex.murataev@gmail.com>
2015-08-26 19:59:51 +0300

    + first commit. much to do :D

---
+++ live.clj
@@ -0,0 +1,8 @@
+(ns ccw-plugin.live
+  (:require [ccw.eclipse :as e]
+            [ccw.e4.dsl :refer :all]))
+
+(defcommand greeter "Hello World from Counterclockwise" "Alt+U H"
+  [context]
+  (e/info-dialog 
+    "Hello world from Counterclockwise" "This popup provided to you from a user script!"))
